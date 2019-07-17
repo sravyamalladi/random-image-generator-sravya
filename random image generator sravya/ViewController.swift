f46d6550a3b67c04569565a64d99cc37e57b8fcc
sravya <sravya@sraviair.local>
2019-07-17 12:38:56 -0700

    final version

--- random image generator sravya/ViewController.swift
+++ random image generator sravya/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+@IBOutlet var image1: UIImageView!
+    var show1: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func imageclick(_ sender: Any) {
+        if (show1 == false){
+        image1.image = UIImage(named: "Book")
+        show1 = true
+        }else{
+        image1.image = UIImage(named: "Bear")
+        show1 = false
+        }
+    
 }
 
+}
